9fa0288d302893457144037161f04268a93f214a
Elvin Shrestha <elwyncrestha@gmail.com>
2022-04-18 12:24:28 +0545

    Added day 3 code snippets

--- day3/function.js
+++ day3/function.js
@@ -1,10 +1,21 @@
 function printFullName() {
+    console.log(arguments);
     console.log(this);
     console.log(`${this.firstName} ${this.lastName}`);
 }
 
+printFullName.call({
+    firstName: 'John',
+    lastName: 'Doe'
+}, 'other', 'params', 'if', 'required');
+
+printFullName.apply({
+    firstName: 'John',
+    lastName: 'Doe'
+}, ['other', 'params', 'if', 'required']);
+
 const printEmployee = printFullName.bind({
     firstName: 'John',
     lastName: 'Doe'
-});
+}, 'other', 'params', 'if', 'required');
 printEmployee();
